85e6b339fda32b6780eee6be4e5077df487c4625
winstonchuckle <bennywarren@gmailcom>
2023-08-03 23:52:59 +0000

    Change 1

--- set2/exercise0.py
+++ set2/exercise0.py
@@ -82,7 +82,7 @@ def really_shout(a_string):
     that you've reused the function you already wrote.
     Look up how to 'concatinate' strings to make this happen.
     """
-    the_answer = None
+    the_answer = a_string.upper()+'!'
     return the_answer
 
 
@@ -98,7 +98,7 @@ def shout_with_a_number(a_string, a_number):
           challenge, see if you can make the test pass with at least two ways
           of doing the same job.
     """
-    the_answer = None
+    the_answer = a_string.upper() + str( a_number)
     return the_answer
 
 
